1d73964eef8741be919181e6dac048a736a70aba
VeryGoodDev <very_good_dev@outlook.com>
2021-08-28 17:49:22 -0700

    Test call to get weather data

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script lang="ts">
-	export let name: string
+  import { onMount } from "svelte"
+
+  export let name: string
+
+  onMount(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=40.356034699999995&lon=-111.75281389999999&appid=875d258f134df1f1b63378278de4e2e8`
+    )
+      .then((res) => res.json())
+      .then(console.log)
+  })
 </script>
 
 <main>
